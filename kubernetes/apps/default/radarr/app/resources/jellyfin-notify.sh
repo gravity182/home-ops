a2e5fd49e2dcf58cbd0a48de4f59395afff524d3
gravity182 <dm.blink19@gmail.com>
2026-01-13 16:19:21 +0300

    Disable Flux var substitution on specific manifests

--- kubernetes/apps/default/radarr/app/resources/jellyfin-notify.sh
+++ kubernetes/apps/default/radarr/app/resources/jellyfin-notify.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-event_type="$${sonarr_eventtype}$${radarr_eventtype}"
+event_type="${sonarr_eventtype}${radarr_eventtype}"
 
 if [ "$event_type" = "Test" ]; then
   echo "Test successful"
   exit 0
 fi
 
-curl -sX POST "http://jellyfin:8096/Items/$${JELLYFIN_LIBRARY_ID}/Refresh?Recursive=true&api_key=$${JELLYFIN_API_KEY}" \
+curl -sX POST "http://jellyfin:8096/Items/${JELLYFIN_LIBRARY_ID}/Refresh?Recursive=true&api_key=${JELLYFIN_API_KEY}" \
   -H "Content-Type: application/json" \
   -H "Content-Length: 0" > /dev/null
